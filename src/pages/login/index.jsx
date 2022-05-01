3f6678a27a03002aea4e5b7e72272f4cba284a40
milenaalcantara <milena.lima.alcantara61@aluno.ifce.edu.br>
2022-05-01 17:39:09 -0300

    create form login

--- src/pages/login/index.jsx
+++ src/pages/login/index.jsx
@@ -1,11 +1,36 @@
 import React from 'react'
 
+import './styles.css'
+
 const LoginPage = () => {
-    return (
-        <div id="login">
-            <h1 className="title">Login do sistema</h1>
+  return (
+    <div id="login">
+      <h1 className="title">Login do sistema</h1>
+      <form className="form">
+        <div className="field">
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            name="email"
+            id="email"
+            placeholder="Digite aqui..."
+          />
         </div>
-    )
+        <div className="field">
+          <label htmlFor="password">Senha</label>
+          <input
+            type="password"
+            name="password"
+            id="password"
+            placeholder="Digite aqui..."
+          />
+        </div>
+        <div className="actions">
+          <button type="submit">Entrar</button>
+        </div>
+      </form>
+    </div>
+  )
 }
 
 export default LoginPage
