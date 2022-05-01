24cb5bcb3237312c912a83d869faa11f28a49920
milenaalcantara <milena.lima.alcantara61@aluno.ifce.edu.br>
2022-05-01 16:56:44 -0300

    created initial file from login page

---
+++ src/pages/login/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const LoginPage = () => {
+    return (
+        <div id="login">
+            <h1 className="title">Login do sistema</h1>
+        </div>
+    )
+}
+
+export default LoginPage
